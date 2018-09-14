f22a2abf8cc0c3eeb5d462ab6705fc23b964f026
Mariusz Nowak <medyk@medikoo.com>
2018-09-14 09:34:08 +0200

    style: prettier

--- test/lib/find-root.js
+++ test/lib/find-root.js
@@ -8,8 +8,7 @@ var fs        = require("fs")
   , mkdir     = promisify(fs.mkdir)
   , rmdir     = promisify(fs.rmdir)
   , gitMode   = require("../../lib/ignore-modes/git")
-
-  , rootPath = resolve(__dirname, "../__playground/lib/find-root");
+  , rootPath  = resolve(__dirname, "../__playground/lib/find-root");
 
 module.exports = function (t, a, d) {
 	var gitRoot = resolve(rootPath, ".git")
@@ -17,82 +16,85 @@ module.exports = function (t, a, d) {
 	  , gitOnePath = resolve(onePath, ".git")
 	  , twoPath = resolve(onePath, "two")
 	  , gitTwoPath = resolve(twoPath, ".git")
-	  , filePath = resolve(twoPath, "file.xxx")
-	  , DELAY = 100
+	  , filePath = resolve(twoPath, "file.xxx");
 
-	  , watcher, events = [];
+	var DELAY = 100, watcher, events = [];
 
 	// Create /.git
 	mkdir(gitRoot)(function () {
-
 		// Create /one
 		return mkdir(onePath);
 	})(function () {
-
 		// Create /one/.git
 		return mkdir(gitOnePath);
 	})(function () {
 		watcher = t(gitMode.isRootWatcher, filePath, { watch: true });
-		watcher.on("change", function (path) {
-			events.push(path);
-		});
+		watcher.on("change", function (path) { events.push(path); });
 		return watcher;
-	})(function (path) {
-		a(path, onePath, "#1");
-	})(delay(function () {
-		a(String(events), "", "#1: Event");
-		events = [];
+	})(function (path) { a(path, onePath, "#1"); })(
+		delay(function () {
+			a(String(events), "", "#1: Event");
+			events = [];
 
-		// Create /one/two
-		return mkdir(twoPath);
-	}, DELAY))(delay(function () {
-
-		// Create /one/two/.git
-		return mkdir(gitTwoPath);
-	}, DELAY))(delay(function () {
-		a(String(events), twoPath, "#2: Event");
-		events = [];
-		return t(gitMode.isRoot, filePath);
-	}, DELAY))(function (path) {
+			// Create /one/two
+			return mkdir(twoPath);
+		}, DELAY)
+	)(
+		delay(function () {
+			// Create /one/two/.git
+			return mkdir(gitTwoPath);
+		}, DELAY)
+	)(
+		delay(function () {
+			a(String(events), twoPath, "#2: Event");
+			events = [];
+			return t(gitMode.isRoot, filePath);
+		}, DELAY)
+	)(function (path) {
 		a(path, twoPath, "#2");
 
 		// Remove /one/.git
 		return rmdir(gitOnePath);
-	})(delay(function () {
-
-		// Remove /one/two/.git
-		return rmdir(gitTwoPath);
-	}, DELAY))(delay(function () {
-		a(String(events), rootPath, "#3: Event");
-		events = [];
-		return t(gitMode.isRoot, filePath);
-	}, DELAY))(function (path) {
+	})(
+		delay(function () {
+			// Remove /one/two/.git
+			return rmdir(gitTwoPath);
+		}, DELAY)
+	)(
+		delay(function () {
+			a(String(events), rootPath, "#3: Event");
+			events = [];
+			return t(gitMode.isRoot, filePath);
+		}, DELAY)
+	)(function (path) {
 		a(path, rootPath, "#3");
 
 		// Create /one/two/.git
 		return mkdir(gitTwoPath);
-	})(delay(function () {
-		a(String(events), twoPath, "#4: Event");
-		events = [];
-		return t(gitMode.isRoot, filePath);
-	}, DELAY))(function (path) {
+	})(
+		delay(function () {
+			a(String(events), twoPath, "#4: Event");
+			events = [];
+			return t(gitMode.isRoot, filePath);
+		}, DELAY)
+	)(function (path) {
 		a(path, twoPath, "#4");
 
 		// Remove /one/two/.git
 		return rmdir(gitTwoPath);
 	})(function () {
-
 		// Remove /one/two
 		return rmdir(twoPath);
 	})(function () {
-
 		// Remove /one
 		return rmdir(onePath);
-	})(delay(function () {
-		a(String(events), rootPath, "#5: Event");
-		events = [];
-		return t(gitMode.isRoot, filePath);
-	}, DELAY))(function (path) {
+	})(
+		delay(function () {
+			a(String(events), rootPath, "#5: Event");
+			events = [];
+			return t(gitMode.isRoot, filePath);
+		}, DELAY)
+	)(function (path) {
 		a(path, rootPath, "#5");
 
 		watcher.close();
