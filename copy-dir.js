cd4e2c638026cedd77f418bf23c8de456c5455e7
Mariusz Nowak <medyk@medikoo.com>
2021-11-16 13:25:25 +0100

    refactor: Fix lint issues

--- copy-dir.js
+++ copy-dir.js
@@ -1,56 +1,53 @@
 "use strict";
 
-var isCallable = require("es5-ext/object/is-callable")
-  , isValue    = require("es5-ext/object/is-value")
-  , startsWith = require("es5-ext/string/#/starts-with")
-  , deferred   = require("deferred")
-  , path       = require("path")
-  , lstat      = require("./lstat")
-  , mkdir      = require("./mkdir")
-  , readdir    = require("./readdir")
-  , readlink   = require("./readlink")
-  , symlink    = require("./symlink")
-  , copyFile   = require("./copy").copy;
+const isCallable = require("es5-ext/object/is-callable")
+    , isValue    = require("es5-ext/object/is-value")
+    , startsWith = require("es5-ext/string/#/starts-with")
+    , deferred   = require("deferred")
+    , path       = require("path")
+    , lstat      = require("./lstat")
+    , mkdir      = require("./mkdir")
+    , readdir    = require("./readdir")
+    , readlink   = require("./readlink")
+    , symlink    = require("./symlink")
+    , copyFile   = require("./copy").copy;
 
-var dirname = path.dirname, relative = path.relative, resolve = path.resolve, sep = path.sep;
-
-var copyDir = function (source, dest, options, sourceTop, destTop) {
+const copyDir = function (source, dest, options, sourceTop, destTop) {
 	return readdir(source, {
 		type: { directory: true, file: true, symbolicLink: true },
 		depth: Infinity
 	})(
-		function (files) {
-			return deferred.map(files, function (relativePath) {
-				var filename = resolve(source, relativePath);
-				return lstat(filename)(function (stats) {
+		files =>
+			deferred.map(files, relativePath => {
+				const filename = path.resolve(source, relativePath);
+				return lstat(filename)(stats => {
 					if (stats.isDirectory()) {
-						return mkdir(resolve(dest, relativePath), { intermediate: true });
+						return mkdir(path.resolve(dest, relativePath), { intermediate: true });
 					}
 					if (stats.isFile()) {
-						return copyFile(filename, resolve(dest, relativePath), {
+						return copyFile(filename, path.resolve(dest, relativePath), {
 							intermediate: true
 						});
 					}
 					if (!stats.isSymbolicLink()) return null;
-					return readlink(filename)(function (linkPath) {
-						linkPath = resolve(dirname(filename), linkPath);
-						var linkDirname = dirname(linkPath);
+					return readlink(filename)(linkPath => {
+						linkPath = path.resolve(path.dirname(filename), linkPath);
+						const linkDirname = path.dirname(linkPath);
 						if (
 							linkDirname === sourceTop ||
-							startsWith.call(linkDirname, sourceTop + sep)
+							startsWith.call(linkDirname, sourceTop + path.sep)
 						) {
-							linkPath = resolve(destTop, linkPath.slice(sourceTop.length + 1));
+							linkPath = path.resolve(destTop, linkPath.slice(sourceTop.length + 1));
 						}
 						return symlink(
-							relative(dirname(resolve(dest, relativePath)), linkPath),
-							resolve(dest, relativePath),
+							path.relative(path.dirname(path.resolve(dest, relativePath)), linkPath),
+							path.resolve(dest, relativePath),
 							{ intermediate: true }
 						);
 					});
 				});
-			});
-		},
-		function (error) {
+			}),
+		error => {
 			if (options.loose && error.code === "ENOENT") return false;
 			throw error;
 		}
@@ -58,16 +55,15 @@ var copyDir = function (source, dest, options, sourceTop, destTop) {
 };
 copyDir.returnsPromise = true;
 
-module.exports = exports = function (source, dest /*, options, cb*/) {
-	var options = Object(arguments[2]), cb = arguments[3];
+module.exports = exports = function (source, dest, options = null, cb = null) {
 	if (!isValue(cb) && isCallable(options)) {
 		cb = options;
 		options = {};
 	}
 
-	source = resolve(String(source));
-	dest = resolve(String(dest));
-	return lstat(dest, { loose: true })(function (stats) {
+	source = path.resolve(String(source));
+	dest = path.resolve(String(dest));
+	return lstat(dest, { loose: true })(stats => {
 		if (stats) throw new Error("Destination path exists");
 		return copyDir(source, dest, options, source, dest);
 	}).cb(cb);
