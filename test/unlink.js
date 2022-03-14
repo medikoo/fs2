0696308f033d04a106334792101fb330576bb385
Mariusz Nowak <medyk@medikoo.com>
2022-03-14 12:57:23 +0100

    style: Clear ESLint errors

--- test/unlink.js
+++ test/unlink.js
@@ -1,18 +1,20 @@
 "use strict";
 
-var lstat     = require("../lstat")
-  , writeFile = require("../write-file")
-  , testFile  = require("path").resolve(__dirname, "__playground/unlink");
+const lstat     = require("../lstat")
+    , writeFile = require("../write-file")
+    , testFile  = require("path").resolve(__dirname, "__playground/unlink");
 
 module.exports = function (t, a, d) {
-	return writeFile(testFile, "foo")(function () {
-		return lstat(testFile)(function () {
-			return t(testFile)(function () {
-				return lstat(testFile)(a.never, function (err) {
+	return writeFile(
+		testFile, "foo"
+	)(() =>
+		lstat(testFile)(() =>
+			t(testFile)(() =>
+				lstat(testFile)(a.never, err => {
 					a(err.code, "ENOENT");
-					return t(testFile + "bla", { loose: true }).catch(a.never);
-				});
-			});
-		});
-	}).done(d, d);
+					return t(`${ testFile }bla`, { loose: true }).catch(a.never);
+				})
+			)
+		)
+	).done(d, d);
 };
