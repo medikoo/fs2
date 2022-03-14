6cd7026b9f66b961bbfdd411be997f3b648c16b7
Mariusz Nowak <medyk@medikoo.com>
2022-03-14 13:03:44 +0100

    refactor: Reuse `emptyDirSync` logic in `rmDirSync`

--- rmdir-sync.js
+++ rmdir-sync.js
@@ -6,24 +6,6 @@ const fs            = require("fs")
     , isObject      = require("type/object/is")
     , toShortString = require("type/lib/to-short-string");
 
-const removeDir = dirname => {
-	let errors;
-	for (let filename of fs.readdirSync(dirname)) {
-		filename = path.resolve(dirname, filename);
-		try {
-			const stats = fs.statSync(filename);
-			if (stats.isDirectory()) removeDir(filename);
-			else fs.unlinkSync(filename);
-		} catch (error) {
-			if (!errors) errors = [];
-			if (error.errors) errors.push(...error.errors);
-			else errors.push(Object.assign(error, { _filename: filename }));
-		}
-	}
-	if (errors) throw Object.assign(new Error("Could not remove dir"), { errors });
-	else fs.rmdirSync(dirname);
-};
-
 module.exports = (dirnameInput, options = {}) => {
 	const dirname = path.resolve(ensureString(dirnameInput));
 	if (!isObject(options)) options = {};
@@ -32,7 +14,8 @@ module.exports = (dirnameInput, options = {}) => {
 		return;
 	}
 	try {
-		removeDir(dirname);
+		emptyDirSync(dirname, options);
+		fs.rmdirSync(dirname);
 	} catch (error) {
 		if (!error.errors) throw error;
 		let errorMessage = `Could not remove ${ toShortString(dirnameInput) } due to:\n- ${
@@ -44,3 +27,5 @@ module.exports = (dirnameInput, options = {}) => {
 		throw Object.assign(new Error(errorMessage), { errors: error.errors });
 	}
 };
+
+const emptyDirSync = require("./empty-dir-sync");
