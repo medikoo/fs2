8de46f13b43aeca216941c4a55aab586a48ab1d1
Mariusz Nowak <medyk@medikoo.com>
2023-08-24 22:16:23 +0200

    test: Refactor to ES2015

--- test/symlink.js
+++ test/symlink.js
@@ -1,29 +1,31 @@
 "use strict";
 
-var promisify = require("deferred").promisify
-  , fs        = require("fs")
-  , path      = require("path")
-  , resolve   = path.resolve
-  , lstat     = promisify(fs.lstat)
-  , unlink    = promisify(fs.unlink)
-  , rootPath  = resolve(__dirname, "./__playground/symlink")
-  , base      = resolve(rootPath, "from")
-  , regular   = resolve(rootPath, "foo")
-  , deep      = resolve(rootPath, "foo/bar");
+const { promisify } = require("deferred")
+    , fs            = require("fs")
+    , path          = require("path")
+    , { resolve }   = path
+    , lstat         = promisify(fs.lstat)
+    , unlink        = promisify(fs.unlink)
+    , rootPath      = resolve(__dirname, "./__playground/symlink")
+    , base          = resolve(rootPath, "from")
+    , regular       = resolve(rootPath, "foo")
+    , deep          = resolve(rootPath, "foo/bar");
 
 module.exports = function (t) {
 	return {
 		Regular: {
-			Success: function (a, d) {
-				t(base, regular)(function () {
-					return lstat(regular)(function (stats) {
+			Success(a, d) {
+				t(
+					base, regular
+				)(() =>
+					lstat(regular)(stats => {
 						a(stats.isSymbolicLink(), true);
 						return unlink(regular);
-					});
-				}).done(d, d);
+					})
+				).done(d, d);
 			},
-			Error: function (a, d) {
-				t(base, deep)(a.never, function () { a.ok(true, ""); }).done(d, d);
+			Error(a, d) {
+				t(base, deep)(a.never, () => { a.ok(true, ""); }).done(d, d);
 			}
 		}
 	};
