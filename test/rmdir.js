6cfe25f5e4c7ab9371fd433e9cd890b0f5e9eff3
Mariusz Nowak <medyk@medikoo.com>
2023-08-25 20:59:02 +0200

    test: Refactor to ES2015

--- test/rmdir.js
+++ test/rmdir.js
@@ -1,47 +1,33 @@
 "use strict";
 
-var resolve   = require("path").resolve
-  , lstat     = require("../lstat")
-  , mkdir     = require("../mkdir")
-  , writeFile = require("../write-file")
-  , pg        = resolve(__dirname, "__playground")
-  , rootPath  = resolve(pg, "rmdir")
-  , nested    = resolve(rootPath, "one/two/three/dir")
-  , nested2   = resolve(rootPath, "one/four/five/six");
+const { resolve } = require("path")
+    , lstat       = require("../lstat")
+    , mkdir       = require("../mkdir")
+    , writeFile   = require("../write-file")
+    , pg          = resolve(__dirname, "__playground")
+    , rootPath    = resolve(pg, "rmdir")
+    , nested      = resolve(rootPath, "one/two/three/dir")
+    , nested2     = resolve(rootPath, "one/four/five/six");
 
 module.exports = function (t, a, d) {
-	mkdir(nested, { intermediate: true })(mkdir(nested2, { intermediate: true }))(function () {
-		return writeFile(resolve(nested, "../foo"), "bar");
-	})(function () {
-		return t(
-			rootPath
-		)(a.never, function (err) {
-			a(err.code, "ENOTEMPTY", "Default on not empty");
-		})(function () {
-			return t(rootPath, {
-				recursive: true
-			})(a.never, function (err) { a(err.code, "ENOTEMPTY", "Recursive not empty"); });
-		})(function () {
-			var path = resolve(rootPath, "one/four");
-			return t(path, { recursive: true })(function () {
-				return lstat(
-					path
-				)(a.never, function (err) { a(err.code, "ENOENT", "Recursive on empty"); });
-			});
-		})(function () {
-			return t(nested)(function () {
-				return lstat(nested)(a.never, function (err) { a(err.code, "ENOENT", "Plain"); });
-			});
-		})(function () {
-			return t(nested, {
-				loose: true
-			})(function (res) { a(res, null, "Loose option"); }, a.never);
-		})(function () {
-			return t(rootPath, { recursive: true, force: true })(function () {
-				return lstat(
-					rootPath
-				)(a.never, function (err) { a(err.code, "ENOENT", "Recursive and forced"); });
-			});
-		});
-	}).done(d, d);
+	mkdir(nested, { intermediate: true })(mkdir(nested2, { intermediate: true }))(() =>
+		writeFile(resolve(nested, "../foo"), "bar")
+	)(() =>
+		t(rootPath)(a.never, err => { a(err.code, "ENOTEMPTY", "Default on not empty"); })(() =>
+			t(rootPath, { recursive: true })(a.never, err => {
+				a(err.code, "ENOTEMPTY", "Recursive not empty");
+			})
+		)(() => {
+			const path = resolve(rootPath, "one/four");
+			return t(path, { recursive: true })(() =>
+				lstat(path)(a.never, err => { a(err.code, "ENOENT", "Recursive on empty"); })
+			);
+		})(() =>
+			t(nested)(() => lstat(nested)(a.never, err => { a(err.code, "ENOENT", "Plain"); }))
+		)(() => t(nested, { loose: true })(res => { a(res, null, "Loose option"); }, a.never))(() =>
+			t(rootPath, { recursive: true, force: true })(() =>
+				lstat(rootPath)(a.never, err => { a(err.code, "ENOENT", "Recursive and forced"); })
+			)
+		)
+	).done(d, d);
 };
