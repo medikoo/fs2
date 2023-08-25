b10e36426dc7430834ccd4cafe975d8d051cd374
Mariusz Nowak <medyk@medikoo.com>
2023-08-25 20:53:57 +0200

    refactor: To ES2015

--- rmdir.js
+++ rmdir.js
@@ -1,73 +1,68 @@
 "use strict";
 
-var partial    = require("es5-ext/function/#/partial")
-  , isCallable = require("es5-ext/object/is-callable")
-  , isValue    = require("es5-ext/object/is-value")
-  , deferred   = require("deferred")
-  , fs         = require("fs")
-  , pathUtils  = require("path")
-  , chmod      = require("./chmod")
-  , lstat      = require("./lstat")
-  , readdir    = require("./readdir")
-  , unlink     = require("./unlink");
+const partial    = require("es5-ext/function/#/partial")
+    , isCallable = require("es5-ext/object/is-callable")
+    , isValue    = require("es5-ext/object/is-value")
+    , deferred   = require("deferred")
+    , fs         = require("fs")
+    , pathUtils  = require("path")
+    , chmod      = require("./chmod")
+    , lstat      = require("./lstat")
+    , readdir    = require("./readdir")
+    , unlink     = require("./unlink");
 
-var original = fs.rmdir
-  , resolve = pathUtils.resolve
-  , sep = pathUtils.sep
-  , rmdir
-  , rmcontent
-  , empty = {};
+const original = fs.rmdir, { resolve } = pathUtils, { sep } = pathUtils, empty = {};
 
-rmcontent = function (path, options) {
+const rmcontent = function (path, options) {
 	return readdir(path)(
 		function self(files, repeated) {
-			return deferred.map(files, function (name) {
-				var filename = path + sep + name, aborted;
+			return deferred.map(files, name => {
+				const filename = path + sep + name;
+				let aborted;
 				return lstat(filename)(
-					function (stats) {
-						var err;
+					stats => {
 						if (aborted || options.aborted) return null;
 						if (stats.isDirectory()) {
-							return rmcontent(filename, options)(null, function (err2) {
+							return rmcontent(filename, options)(null, err2 => {
 								options.aborted = true;
 								throw err2;
 							});
 						}
 						if (options.force) {
-							return unlink(filename)(null, function (err2) {
+							return unlink(filename)(null, err2 => {
 								if (err2.code === "ENOENT") return;
 								aborted = true;
 								throw err2;
 							});
 						}
-						err = new Error("ENOTEMPTY rmdir '" + path + "'");
+						const err = new Error(`ENOTEMPTY rmdir '${ path }'`);
 						err.errno = 53;
 						err.code = "ENOTEMPTY";
 						err.path = path;
 						options.aborted = true;
 						throw err;
 					},
-					function (err) {
+					err => {
 						if (err.code === "ENOENT") return;
 						options.aborted = true;
 						throw err;
 					}
 				);
-			})(null, function (err) {
+			})(null, err => {
 				if (!options.aborted && !repeated && err.code === "EACCES" && chmod) {
 					return chmod(path, 146)(partial.call(self, files, true));
 				}
 				throw err;
 			});
 		},
-		function (err) {
+		err => {
 			if (err.code === "ENOENT") return;
 			throw err;
 		}
 	)(
-		function () {
+		() => {
 			if (options.aborted) return null;
-			return rmdir(path, empty)(null, function (err) {
+			return rmdir(path, empty)(null, err => {
 				if (options.aborted) return null;
 				if (err.code === "ENOTEMPTY") {
 					// Race condition (new files were added to the directory in a meantime)
@@ -77,16 +72,16 @@ rmcontent = function (path, options) {
 				throw err;
 			});
 		},
-		function (err) {
+		err => {
 			if (err.code === "ENOENT") return;
 			throw err;
 		}
 	);
 };
 
-rmdir = function (path, options) {
-	var def = deferred();
-	original(path, function (err) {
+const rmdir = function (path, options) {
+	const def = deferred();
+	original(path, err => {
 		if (err) {
 			if (err.code === "ENOTEMPTY") {
 				if (options.recursive) {
@@ -108,12 +103,8 @@ rmdir = function (path, options) {
 };
 rmdir.returnsPromise = true;
 
-module.exports = exports = function (path/*, options, callback*/) {
-	var options, cb;
-
+module.exports = exports = function (path, options = {}, cb = null) {
 	path = resolve(String(path));
-	options = arguments[1];
-	cb = arguments[2];
 
 	if (!isValue(cb) && isCallable(options)) {
 		cb = options;
