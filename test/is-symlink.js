0479273cff4d0c65d670144cb689261d6dc30323
Mariusz Nowak <medyk@medikoo.com>
2023-08-26 19:43:19 +0200

    test: Improve test readability

--- test/is-symlink.js
+++ test/is-symlink.js
@@ -11,25 +11,33 @@ const rootPath = resolve(__dirname, "./__playground/is-symlink")
     , symlink2 = resolve(rootPath, "sym2");
 
 module.exports = function (t, a, d) {
-	symlink(
-		"from", symlink1
-	)(() => symlink("sym1", symlink2))(() => t(symlink2))(result => {
-		a(result, true);
-		return t(symlink2, { linkPath: targetFile });
-	})(result => {
-		a(result, false);
-		return t(symlink2, { linkPath: symlink1 });
-	})(result => {
-		a(result, true);
-		return t(symlink2, { linkPath: targetFile, recursive: true });
-	})(result => {
-		a(result, true);
-		return t(targetFile);
-	})(result => {
-		a(result, false);
-		return t(`${ targetFile }/foo`);
-	})(result => {
-		a(result, null);
-		return unlink(symlink1)(unlink(symlink2));
-	})(() => t(nonSymlink, { loose: true }))(result => { a(result, null); }).done(d, d);
+	symlink("from", symlink1)
+		.then(() => symlink("sym1", symlink2))
+		.then(() => t(symlink2))
+		.then(result => {
+			a(result, true);
+			return t(symlink2, { linkPath: targetFile });
+		})
+		.then(result => {
+			a(result, false);
+			return t(symlink2, { linkPath: symlink1 });
+		})
+		.then(result => {
+			a(result, true);
+			return t(symlink2, { linkPath: targetFile, recursive: true });
+		})
+		.then(result => {
+			a(result, true);
+			return t(targetFile);
+		})
+		.then(result => {
+			a(result, false);
+			return t(`${ targetFile }/foo`);
+		})
+		.then(result => {
+			a(result, null);
+			return unlink(symlink1)(unlink(symlink2));
+		})
+		.then(() => t(nonSymlink, { loose: true }))(result => { a(result, null); })
+		.done(d, d);
 };
