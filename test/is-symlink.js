7d5e4b1845b087d122482da67298ffe7ed002c5c
Mariusz Nowak <medyk@medikoo.com>
2023-08-26 19:41:20 +0200

    test: Refactor to ES2015

--- test/is-symlink.js
+++ test/is-symlink.js
@@ -1,39 +1,35 @@
 "use strict";
 
-var resolve = require("path").resolve
-  , symlink = require("../symlink")
-  , unlink  = require("../unlink");
+const { resolve } = require("path")
+    , symlink     = require("../symlink")
+    , unlink      = require("../unlink");
 
-var rootPath = resolve(__dirname, "./__playground/is-symlink")
-  , targetFile = resolve(rootPath, "from")
-  , nonSymlink = resolve(rootPath, "non-symlink")
-  , symlink1 = resolve(rootPath, "sym1")
-  , symlink2 = resolve(rootPath, "sym2");
+const rootPath = resolve(__dirname, "./__playground/is-symlink")
+    , targetFile = resolve(rootPath, "from")
+    , nonSymlink = resolve(rootPath, "non-symlink")
+    , symlink1 = resolve(rootPath, "sym1")
+    , symlink2 = resolve(rootPath, "sym2");
 
 module.exports = function (t, a, d) {
 	symlink(
 		"from", symlink1
-	)(function () { return symlink("sym1", symlink2); })(function () { return t(symlink2); })(
-		function (result) {
-			a(result, true);
-			return t(symlink2, { linkPath: targetFile });
-		}
-	)(function (result) {
+	)(() => symlink("sym1", symlink2))(() => t(symlink2))(result => {
+		a(result, true);
+		return t(symlink2, { linkPath: targetFile });
+	})(result => {
 		a(result, false);
 		return t(symlink2, { linkPath: symlink1 });
-	})(function (result) {
+	})(result => {
 		a(result, true);
 		return t(symlink2, { linkPath: targetFile, recursive: true });
-	})(function (result) {
+	})(result => {
 		a(result, true);
 		return t(targetFile);
-	})(function (result) {
+	})(result => {
 		a(result, false);
-		return t(targetFile + "/foo");
-	})(function (result) {
+		return t(`${ targetFile }/foo`);
+	})(result => {
 		a(result, null);
 		return unlink(symlink1)(unlink(symlink2));
-	})(function () { return t(nonSymlink, { loose: true }); })(function (result) {
-		a(result, null);
-	}).done(d, d);
+	})(() => t(nonSymlink, { loose: true }))(result => { a(result, null); }).done(d, d);
 };
