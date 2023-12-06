22e8cbe6b121f5cf55191e1bf61774d2125e7c54
Mariusz Nowak <medyk@medikoo.com>
2023-12-06 17:30:52 +0100

    refactor: Refactor to ES2015

--- is-ignored.js
+++ is-ignored.js
@@ -2,48 +2,40 @@
 
 "use strict";
 
-var invoke         = require("es5-ext/function/invoke")
-  , noop           = require("es5-ext/function/noop")
-  , isCallable     = require("es5-ext/object/is-callable")
-  , isValue        = require("es5-ext/object/is-value")
-  , forEach        = require("es5-ext/object/for-each")
-  , memoize        = require("memoizee")
-  , deferred       = require("deferred")
-  , ignore         = require("ignore")
-  , pathUtils      = require("path")
-  , modes          = require("./lib/ignore-modes")
-  , getMap         = require("./lib/get-conf-file-map")
-  , memoizeWatcher = require("./lib/memoize-watcher")
-  , findRoot       = require("./lib/find-root");
+const invoke         = require("es5-ext/function/invoke")
+    , noop           = require("es5-ext/function/noop")
+    , isCallable     = require("es5-ext/object/is-callable")
+    , isValue        = require("es5-ext/object/is-value")
+    , forEach        = require("es5-ext/object/for-each")
+    , memoize        = require("memoizee")
+    , deferred       = require("deferred")
+    , ignore         = require("ignore")
+    , pathUtils      = require("path")
+    , modes          = require("./lib/ignore-modes")
+    , getMap         = require("./lib/get-conf-file-map")
+    , memoizeWatcher = require("./lib/memoize-watcher")
+    , findRoot       = require("./lib/find-root");
 
-var isArray = Array.isArray
-  , push = Array.prototype.push
-  , call = Function.prototype.call
-  , trim = call.bind(String.prototype.trim)
-  , dirname = pathUtils.dirname
-  , resolve = pathUtils.resolve
-  , sep = pathUtils.sep
-  , ConfMap = getMap.ConfMap
-  , applyRules
-  , applyGlobalRules
-  , compile
-  , IsIgnored
-  , isIgnored
-  , buildMap
-  , prepareRules
-  , parseSrc
-  , eolRe = /(?:\r\n|[\n\r\u2028\u2029])/;
+const { isArray } = Array
+    , { push } = Array.prototype
+    , { call } = Function.prototype
+    , trim = call.bind(String.prototype.trim)
+    , { dirname } = pathUtils
+    , { resolve } = pathUtils
+    , { sep } = pathUtils
+    , { ConfMap } = getMap
+    , eolRe = /(?:\r\n|[\n\r\u2028\u2029])/u;
 
-prepareRules = function (data) { return data.map(trim).filter(Boolean).reverse(); };
+const prepareRules = function (data) { return data.map(trim).filter(Boolean).reverse(); };
 
-parseSrc = function (src) { return prepareRules(String(src).split(eolRe)); };
+const parseSrc = function (src) { return prepareRules(String(src).split(eolRe)); };
 
-compile = function (maps, result) {
-	var data = (result.data = {}), paths = (result.paths = []);
+const compile = function (maps, result) {
+	const data = (result.data = {}), paths = (result.paths = []);
 
 	// Merge rules found in ignorefiles
-	maps.forEach(function (map) {
-		forEach(map.map, function (rules, path) {
+	maps.forEach(map => {
+		forEach(map.map, (rules, path) => {
 			if (!rules.length) return;
 			if (!data[path]) {
 				paths.push(path);
@@ -56,17 +48,20 @@ compile = function (maps, result) {
 	return result;
 };
 
-applyRules = function (rules, rootPath, path) {
+const applyRules = function (rules, rootPath, path) {
 	if (!rootPath.endsWith(sep)) rootPath += sep;
 	if (!path.startsWith(rootPath)) return { value: false, target: path };
-	rules = rules.slice().reverse().filter(function (rule) { return !rule.startsWith("#"); });
-	var ig = ignore().add(rules);
-	var testPath = path.slice(rootPath.length);
-	var result = ig.ignores(testPath);
+	rules = rules
+		.slice()
+		.reverse()
+		.filter(rule => !rule.startsWith("#"));
+	const ig = ignore().add(rules);
+	const testPath = path.slice(rootPath.length);
+	let result = ig.ignores(testPath);
 	if (!result) {
-		var excludeRules = rules.filter(function (rule) { return rule.startsWith("!"); });
+		const excludeRules = rules.filter(rule => rule.startsWith("!"));
 		if (excludeRules.length) {
-			var ig2 = ignore().add(excludeRules.map(function (rule) { return rule.slice(1); }));
+			const ig2 = ignore().add(excludeRules.map(rule => rule.slice(1)));
 			if (!ig2.ignores(testPath)) result = null;
 		} else {
 			result = null;
@@ -75,20 +70,19 @@ applyRules = function (rules, rootPath, path) {
 	return { value: result, target: path };
 };
 
-applyGlobalRules = function (path, rules) {
-	var value;
-
+const applyGlobalRules = function (path, rules) {
 	// Check global rules
-	value = applyRules(rules, path.slice(0, path.indexOf(sep) + 1), path);
+	const value = applyRules(rules, path.slice(0, path.indexOf(sep) + 1), path);
 	return Boolean(value.value);
 };
 
-buildMap = function (lDirname, lGetMap, watch) {
-	var promise, data = {}, maps;
-	lGetMap = lGetMap.map(function (getSubMap, index) {
-		var map = getSubMap(lDirname);
+const buildMap = function (lDirname, lGetMap, watch) {
+	let promise, maps;
+	const data = {};
+	lGetMap = lGetMap.map((getSubMap, index) => {
+		const map = getSubMap(lDirname);
 		if (watch) {
-			map.on("change", function (targetMap) {
+			map.on("change", targetMap => {
 				if (maps) {
 					maps[index] = targetMap;
 					compile(maps, data);
@@ -99,12 +93,12 @@ buildMap = function (lDirname, lGetMap, watch) {
 		return map;
 	});
 	if (lGetMap.length > 1) {
-		promise = deferred.map(lGetMap)(function (result) {
+		promise = deferred.map(lGetMap)(result => {
 			maps = result;
 			return compile(maps, data);
 		});
 	} else {
-		promise = lGetMap[0](function (map) {
+		promise = lGetMap[0](map => {
 			maps = [map];
 			return compile(maps, data);
 		});
@@ -115,39 +109,34 @@ buildMap = function (lDirname, lGetMap, watch) {
 	return promise;
 };
 
-IsIgnored = function (path, watch) {
+const IsIgnored = function (path, watch) {
 	this.path = path;
 	this.dirname = dirname(path);
 	this.watch = watch;
 };
 
 IsIgnored.prototype = {
-	init: function (mapPromise) {
+	init(mapPromise) {
 		this.mapPromise = mapPromise;
-		this.promise = mapPromise(
-			function (data) {
-				this.data = data;
-				return this.calculate();
-			}.bind(this)
-		);
+		this.promise = mapPromise(data => {
+			this.data = data;
+			return this.calculate();
+		});
 		if (this.watch) {
-			mapPromise.on(
-				"change",
-				function () {
-					var value = this.calculate();
-					if (value !== this.promise.value) {
-						this.promise.value = value;
-						this.promise.emit("change", value, this.path);
-					}
-				}.bind(this)
-			);
+			mapPromise.on("change", () => {
+				const value = this.calculate();
+				if (value !== this.promise.value) {
+					this.promise.value = value;
+					this.promise.emit("change", value, this.path);
+				}
+			});
 			this.promise.close = this.close.bind(this);
 		}
 		return this.promise;
 	},
-	close: function () { this.mapPromise.close(); },
-	calculate: function () {
-		var current, result = false;
+	close() { this.mapPromise.close(); },
+	calculate() {
+		let current, result = false;
 
 		if (!this.data.paths) return false;
 
@@ -156,15 +145,17 @@ IsIgnored.prototype = {
 		this.data.paths.some(function (rulesPath, index) {
 			if (rulesPath.length > this.dirname.length) return true;
 			if (index) {
-				var dirIgnored = this.data.paths.slice(0, index).some(function (preRulesPath) {
-					return this.data.data[preRulesPath].some(function (rules) {
-						return applyRules(rules, preRulesPath, rulesPath).value;
+				const dirIgnored = this.data.paths
+					.slice(0, index)
+					.some(function (preRulesPath) {
+						return this.data.data[preRulesPath].some(
+							rules => applyRules(rules, preRulesPath, rulesPath).value, this
+						);
 					}, this);
-				}, this);
 				if (dirIgnored) return false;
 			}
 			this.data.data[rulesPath].forEach(function (rules) {
-				var data = applyRules(rules, rulesPath, current);
+				let data = applyRules(rules, rulesPath, current);
 				if (data.value === false && current !== pathUtils) {
 					data = applyRules(rules, rulesPath, this.path);
 				}
@@ -179,8 +170,8 @@ IsIgnored.prototype = {
 	}
 };
 
-isIgnored = function (mode, path, options) {
-	var watch, globalRules, lIsIgnored, getMapFns, lDirname, promise;
+const isIgnored = function (mode, path, options) {
+	let globalRules, getMapFns, promise;
 
 	if (isValue(options.globalRules)) {
 		globalRules = isArray(options.globalRules)
@@ -188,23 +179,24 @@ isIgnored = function (mode, path, options) {
 			: String(options.globalRules).split(eolRe);
 	}
 
+	const { watch } = options;
+	const lIsIgnored = new IsIgnored(path, watch);
+	const lDirname = lIsIgnored.dirname;
+
 	if (mode) {
 		getMapFns = [];
 		if (!globalRules) globalRules = [];
 		if (!isArray(mode)) {
-			if (!modes[mode]) throw new Error("Unknown mode '" + mode + "'");
+			if (!modes[mode]) throw new Error(`Unknown mode '${ mode }'`);
 			mode = [mode];
 		}
-		mode.forEach(function (name) {
-			var lMode = modes[name];
-			if (!lMode) throw new Error("Unknown mode '" + name + "'");
-			getMapFns.push(function (pathIgnored) {
-				return getMap(lDirname, lMode, watch, parseSrc);
-			});
+		mode.forEach(name => {
+			const lMode = modes[name];
+			if (!lMode) throw new Error(`Unknown mode '${ name }'`);
+			getMapFns.push(pathIgnored => getMap(lDirname, lMode, watch, parseSrc));
 			if (lMode.globalRules) push.apply(globalRules, lMode.globalRules);
 		});
 	}
-	watch = options.watch;
 
 	if (globalRules) {
 		globalRules = prepareRules(globalRules);
@@ -221,18 +213,13 @@ isIgnored = function (mode, path, options) {
 		return promise;
 	}
 
-	lIsIgnored = new IsIgnored(path, watch);
-	lDirname = lIsIgnored.dirname;
 	return lIsIgnored.init(buildMap(lDirname, getMapFns, watch));
 };
 isIgnored.returnsPromise = true;
 
-module.exports = exports = function (mode, path) {
-	var options, cb;
+module.exports = exports = function (mode, path, options = {}, cb = null) {
 	path = resolve(String(path));
 
-	options = Object(arguments[2]);
-	cb = arguments[3];
 	if (!cb) {
 		if (isCallable(options)) {
 			cb = options;
@@ -248,32 +235,30 @@ exports.IsIgnored = IsIgnored;
 exports.applyGlobalRules = applyGlobalRules;
 
 exports.getIsIgnored = function (modeNames, globalRules, watch) {
-	var memo, mapGetters = [], build;
+	const mapGetters = [];
 	if (!globalRules) globalRules = [];
-	memo = watch ? memoizeWatcher : memoize;
-	modeNames.forEach(function (name) {
-		var mode = modes[name], isRoot, readRules;
-		if (!mode) throw new Error("Unknown mode '" + name + "'");
-		isRoot = memo(mode[watch ? "isRootWatcher" : "isRoot"], { primitive: true });
-		readRules = memo(getMap[watch ? "readRulesWatcher" : "readRules"], { primitive: true });
-		mapGetters.push(function (path) {
-			var map;
-			map = new ConfMap(path, watch);
+	const memo = watch ? memoizeWatcher : memoize;
+	modeNames.forEach(name => {
+		const mode = modes[name];
+		if (!mode) throw new Error(`Unknown mode '${ name }'`);
+		const isRoot = memo(mode[watch ? "isRootWatcher" : "isRoot"], { primitive: true });
+		const readRules = memo(getMap[watch ? "readRulesWatcher" : "readRules"], {
+			primitive: true
+		});
+		mapGetters.push(path => {
+			const map = new ConfMap(path, watch);
 			map.filename = mode.filename;
 			map.readRules = readRules;
 			map.parse = parseSrc;
-			return map.init(findRoot(isRoot, path, { watch: watch }));
+			return map.init(findRoot(isRoot, path, { watch }));
 		});
 		if (mode.globalRules) push.apply(globalRules, mode.globalRules);
 	});
-	build = memo(function (lDirname) { return buildMap(lDirname, mapGetters, watch); }, {
-		primitive: true
-	});
+	const build = memo(lDirname => buildMap(lDirname, mapGetters, watch), { primitive: true });
 
 	return {
-		isIgnored: function (path) {
-			var lIsIgnored;
-			lIsIgnored = new IsIgnored(path, watch);
+		isIgnored(path) {
+			const lIsIgnored = new IsIgnored(path, watch);
 			return lIsIgnored.init(build(lIsIgnored.dirname));
 		},
 		globalRules: globalRules.length ? prepareRules(globalRules) : null
